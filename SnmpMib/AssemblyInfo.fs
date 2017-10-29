c72f54c04e6a87d6c1c693e683380a09c530d2a7
Konrad Kruś <wiekonek@gmail.com>
2017-10-29 14:32:31 +0100

    Add project files.

---
+++ SnmpMib/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace SnmpMib.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("SnmpMib")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("SnmpMib")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("b9ce0dca-e6f3-46e3-ab8d-262b270625ea")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ SnmpMib/Parser.fs
@@ -0,0 +1,66 @@
+﻿namespace SnmpMib
+
+open System.IO
+open System.Text.RegularExpressions
+open System
+
+module Parsing =
+
+  let private defaultPath = @"C:\\Users\wieko\Documents\mibs\"
+
+  [<Struct>]
+  type RawObjectIdentifier = {
+    Name: string;
+    Oid: string;
+  }
+
+  [<Struct>]
+  type RawObjectType = {
+    Name: string;
+    Syntax: string;
+    Access: string;
+    Status: string;
+    Description: string;
+    Index: string option;
+    Reference: string option;
+    Defval: string option;
+    Oid: string;
+  }
+
+  let private getOid obj =
+    obj.Oid
+
+  let private fileObjectIdentifiers filePath =
+    let fileContent = File.ReadAllText(filePath)
+    let matches = Regex.Matches(fileContent, @"(?m)^(?'name'[\w-]+)\s*OBJECT IDENTIFIER ::= { (?'oid'.*) }")
+    matches
+    |> Seq.cast<Match>
+    |> Seq.map (fun m -> { Name = m.Groups.["name"].Value; Oid = m.Groups.["oid"].Value})
+
+  let private fileObjectTypes filePath =
+    let toRawObjectType (m: Match) =
+      { 
+        Name = m.Groups.["obj"].Value;
+        Syntax = m.Groups.["syntax"].Value;
+        Access = m.Groups.["access"].Value;
+        Status = m.Groups.["status"].Value;
+        Description = m.Groups.["description"].Value;
+        Index = None(*m.Groups.Item("index")*);
+        Reference = None(*m.Groups.Item("index")*);
+        Defval = None(*m.Groups.Item("index")*);
+        Oid = m.Groups.["oid"].Value;
+      }
+    let fileContetn = File.ReadAllText(filePath)
+    let matches = Regex.Matches(
+                    fileContetn,
+                    @"^(?'obj'[\w-]+) OBJECT-TYPE$\s+SYNTAX\s+(?'syntax'.*?)$\s+ACCESS\s+(?'access'.*?)$\s+STATUS\s+(?'status'.*?)$\s+DESCRIPTION\s+\""(?'description'.*?)\""\s+(INDEX\s+{ (?'index'.*?) }\s+)?(REFERENCE\s+\""(?'reference'.*?)\""\s+)?(DEFVAL\s+{ (?'defval'.*?) }\s+)?::= { (?'oid'.+?) }",
+                    RegexOptions.Multiline ||| RegexOptions.Singleline) 
+    matches
+    |> Seq.cast<Match>
+    |> Seq.map toRawObjectType
+
+  type Parser(mibsPath) =
+    member private this.mibsPath = mibsPath
+
+    member public this.Test() =
+      printf "Hello"
